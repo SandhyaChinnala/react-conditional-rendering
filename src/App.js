d5fd53fe26f895ebe1aba8804f4a056ebbe4c100
SandhyaChinnala <o162046@rguktong.ac.in>
2022-12-17 15:23:28 +0000

    conditional rendering

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  //   renderAuthButton = () => {
+  //     const {isLoggedIn} = this.state
+  //     if (isLoggedIn === true) {
+  //       return <button>Logout</button>
+  //     }
+  //     return <button>Login</button>
+  //   }
+
+  render() {
+    const {isLoggedIn} = this.state
+
+    // let authButton
+    // if (isLoggedIn === true) {
+    //   authButton = <button>Logout</button>
+    // } else {
+    //   authButton = <button>Login</button>
+    // }
+
+    return (
+      <div className="container">
+        <Welcome greeting="Hi" />
+        {/* {authButton} */}
+        {/* {isLoggedIn ? <button>Logout</button> : <button>Login</button>} */}
+        {/* {isLoggedIn && <button>Logout</button>}
+        {!isLoggedIn && <button>Login</button>} */}
+      </div>
+    )
+  }
+}
+
+export default App
